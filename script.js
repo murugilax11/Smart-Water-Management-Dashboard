4352966f46e6f233042bae58ba94f17a4d25d019
murugilax11 <murugilax11@gmail.com>
2026-03-16 21:44:07 +0530

    add pages

--- script.js
+++ script.js
@@ -120,7 +120,55 @@ const trendChart = new Chart(trendCtx, {
     }
 });
 
+// ===== MONTHLY CHART =====
 
+document.addEventListener("DOMContentLoaded", function(){
+
+const monthlyCanvas = document.getElementById("monthlyChart");
+
+if(monthlyCanvas){
+
+const monthlyData = [
+210,230,240,260,220,250,270,
+280,260,240,230,250,270,290,
+300,280,260,240,250,270,290,
+310,300,280,260,250,240,230,
+220,210
+];
+
+const ctx = monthlyCanvas.getContext("2d");
+
+new Chart(ctx,{
+
+type:"line",
+
+data:{
+labels:[
+"1","2","3","4","5","6","7","8","9","10",
+"11","12","13","14","15","16","17","18","19","20",
+"21","22","23","24","25","26","27","28","29","30"
+],
+
+datasets:[{
+label:"Monthly Water Usage (Liters)",
+data:monthlyData,
+borderColor:"#0284c7",
+backgroundColor:"rgba(2,132,199,0.2)",
+fill:true,
+tension:0.4
+}]
+},
+
+options:{
+responsive:true,
+maintainAspectRatio:false
+}
+
+});
+
+}
+
+});
 // ===== SIMULATE SENSOR DATA =====
 function updateWaterData() {
 
@@ -213,5 +261,78 @@ if (graphBtn) {
         link.click();
 
     });
+
+}
+
+const menuDashboard = document.getElementById("menuDashboard");
+const menuDaily = document.getElementById("menuDaily");
+const menuMonthly = document.getElementById("menuMonthly");
+const menuSettings = document.getElementById("menuSettings");
+
+const dashboardPage = document.getElementById("dashboardPage");
+const dailyPage = document.getElementById("dailyPage");
+const monthlyPage = document.getElementById("monthlyPage");
+const settingsPage = document.getElementById("settingsPage");
+
+function hidePages(){
+dashboardPage.style.display="none";
+dailyPage.style.display="none";
+monthlyPage.style.display="none";
+settingsPage.style.display="none";
+}
+
+menuDashboard.onclick = function(){
+hidePages();
+dashboardPage.style.display="block";
+}
+
+menuDaily.onclick = function(){
+hidePages();
+dailyPage.style.display="block";
+}
+
+menuMonthly.onclick = function(){
+hidePages();
+monthlyPage.style.display="block";
+}
+
+menuSettings.onclick = function(){
+hidePages();
+settingsPage.style.display="block";
+}
+
+// ===== WATER LIMIT SAVE =====
+
+let waterLimit = localStorage.getItem("waterLimit") || 0;
+
+document.getElementById("saveLimit").addEventListener("click", function(){
+
+waterLimit = Number(document.getElementById("waterLimit").value);
+
+localStorage.setItem("waterLimit", waterLimit);
+
+alert("✅ Water limit saved successfully!");
+
+});
+
+// ===== CHECK LIMIT =====
+
+function checkWaterLimit(){
+
+const notificationEnabled =
+document.getElementById("notificationToggle").checked;
+
+if(notificationEnabled && waterLimit){
+
+if(waterData.totalUsage > waterLimit){
+
+alert("⚠ Warning! Water usage exceeded your limit!");
+
+document.getElementById("alertMessage").innerText =
+"⚠ Water usage exceeded limit!";
+
+}
+
+}
 
 }
